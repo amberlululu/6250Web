ea9b97bcb555bd5e610aaac354c291a6f031001c
amberlululu <57117891+amberlululu@users.noreply.github.com>
2022-02-07 21:18:39 -0800

    Basic js (#2)
    * added .gitignore
    
    * edit compare.js file to make compare funtion work

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -6,5 +6,36 @@ function compare( word, guess ) {  // DO NOT MODIFY
 
 /* YOU MAY MODIFY THE LINES BELOW */
 
-  return 0; // this line is wrong
+  let matched = 0;
+  const objWord = {};
+  const objGuess = {};
+
+  const upWord = word.toUpperCase();
+  const upGuess = guess.toUpperCase();
+
+  for(let i = 0; i< upWord.length; i++){
+    if(!objWord.hasOwnProperty(upWord[i])){
+      objWord[upWord[i]] = 0;
+    }
+    objWord[upWord[i]]++;
+  }
+
+  for(let j = 0; j < upGuess.length; j++){
+    if(!objGuess.hasOwnProperty(upGuess[j])){
+      objGuess[upGuess[j]] = 0;
+    }
+    objGuess[upGuess[j]]++;
+  }
+
+  for(let key in objWord){
+    if(objGuess.hasOwnProperty(key)){
+      if(objWord[key] < objGuess[key]){
+        matched+=objWord[key];
+      }else{
+        matched+=objGuess[key];
+      }
+    }
+  }
+  
+  return matched;
 }
